e0651398e2d2a7e6be05443484ff832a782146a0
mrchav <38419292+mrchav@users.noreply.github.com>
2022-08-15 07:10:25 +0300

    last commit

--- ud_test/flask_app/app.py
+++ ud_test/flask_app/app.py
@@ -22,8 +22,9 @@ migrate = Migrate(app, db)
 #путь к фаилу с доступом к google sheets
 CREDENTIALS_FILE = 'credentials.json'
 
+# фаил google sheets - источник данных.
+# #https://docs.google.com/spreadsheets/d/1K-VI_iqMvKMHpF9TxmyFGutZj6nYjrwARuxbk98lF6s/edit#gid=0
 source_spreadsheet_id = '1K-VI_iqMvKMHpF9TxmyFGutZj6nYjrwARuxbk98lF6s'
-target_spreadsheet_id = '1vra9RoSK2vJ0SEfVVYJwGjxoHVeJ5M_b--TbOE0QGFU'
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name(
     CREDENTIALS_FILE,
@@ -34,69 +35,78 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name(
 )
 
 
-
+# URL при переходе по которому обновляются данные в таблице.
+# Данный URL периодически открывается в задачах, для обновления данных.
+# Надо переписать его через API
 @app.route('/update')
 def udate_page():
+    # Обновляем курс доллога.
     get_usa_rate()
+    # Получаем данные с гугл таблицы. И оставляем все, кроме 1 строчки
     orders_in_source = get_google_sheet_data()['values'][1:]
-
+    # Вместо удаление старых данных, обновляем у них статус на неактивные.
     orders_in_base = Order.query.update({'active': False})
-
+    # Перебираем полученные данные с таблицы и создаем новый объек класса Order на основе полученных данных
     for r in orders_in_source:
         new_order = Order(r[0], r[1], r[2], datetime.strptime(r[3], '%d.%m.%Y'))
         print(f'source id {r[0]} order number{r[1]} cost us {r[2]} date {r[3]}')
         db.session.add(new_order)
         db.session.flush()
 
+    #загружаем новые объекты в БД
     db.session.commit()
-
-
     return 'hi'
 
+# Главная старица сайта, на которой получаем информацию о всех текущих заказах
 @app.route('/')
 def main_page():
+    # Достаем из базы все активные заказы. При необходимости можно ввести сортировку
     orders_in_base = Order.query.filter(Order.active == True).all()
-
+    # Рендерим страницу и передаем в нее все полученные объекты
     return render_template('main_page.html', title='Все операции', orders=orders_in_base)
 
-
+# Парсим данные с Google таблицы
 def get_google_sheet_data():
 
     httpAuth = credentials.authorize(httplib2.Http())
     service = googleapiclient.discovery.build('sheets', 'v4', http=httpAuth)
 
-    values = service.spreadsheets().values().get(
-        spreadsheetId=source_spreadsheet_id,
-        range='Sheet',
-        majorDimension='ROWS'
+    values = service.spreadsheets().values().get(   #запрос к API google
+        spreadsheetId=source_spreadsheet_id,        #фаил к которому обращаемся
+    range='Sheet',                                  #Область, которую забираем - в нашем случае лист
+        majorDimension='ROWS'                       #Порядок расположения данных, в нашем случае как в оригинале - строки
     ).execute()
 
-    #push_google_sheet_data(values['values'])
     return values
 
+# Обновляем курс доллора
 def get_usa_rate():
-
+    # запрос к XML ЦБ. Данные за текущее число
     rn = ET.fromstring(requests.get("http://www.cbr.ru/scripts/XML_daily.asp").text)
+    #перебираем всех "детей", пока не встретим ID интересующей нас валюты
     for child in rn:
         if child.get('ID') == 'R01235':
-            #print(f"курс доллара {child.find('Value').text}")
+            #Забираем курс доллора, меняя на нужный нам формат
             usa_dol_rate = child.find('Value').text
     return float(usa_dol_rate.replace(",","."))
 
-
+# Создаем модель заказа
 class Order(db.Model):
-
+    # Название таблицы в БД данной модели
     __tablename__ = 'orders'
 
-    id = db.Column(db.Integer, primary_key=True)
-    order_number = db.Column(db.Integer, nullable=True)
-    source_id = db.Column(db.Integer, nullable=True)
-    cost_us = db.Column(db.Integer)
-    cost_ru = db.Column(db.Integer)
-    date = db.Column(db.DateTime)
-    update_date = db.Column(db.DateTime)
-    active = db.Column(db.Boolean)
+    id = db.Column(db.Integer, primary_key=True)            # ID в базе
+    order_number = db.Column(db.Integer, nullable=True)     # Номер заказа
+    source_id = db.Column(db.Integer, nullable=True)        # ID в исходной таблице
+    cost_us = db.Column(db.Integer)                         # Стоимость в уе
+    cost_ru = db.Column(db.Integer)                         # Стоимость в рубля
+    date = db.Column(db.DateTime)                           # Дата поставки
+    update_date = db.Column(db.DateTime)                    # Дата обновления информации о заказе
+    active = db.Column(db.Boolean)                          # Активен ли заказ или нет
 
+    #
+    #Конструктор нового обхекта
+    #
     def __init__(self, source_id, order_number, cost_us, date):
         self.source_id = source_id
         self.order_number = order_number
--- ud_test_tasks/tasks/fl_tasks.py
+++ ud_test_tasks/tasks/fl_tasks.py
@@ -1,5 +1,6 @@
 import requests
 
+# задание - открывает страницу web проекта, в результате чего запускается скрипт обновления
 def update_data():
     print('task update_data is start')
     return print(requests.get('http://flask:5000/update'))
